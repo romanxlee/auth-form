a2307eccd014cc4278030cc03e87740ecd8b2d06
romanxlee <romanxlee@yandex.ru>
2021-06-18 13:02:27 +0300

    project setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
+  Vuex,
+  Buefy,
   render: h => h(App),
 }).$mount('#app')
