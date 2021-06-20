28c979c010565524d3292b7314f8df0bc62e8d8c
romanxlee <romanxlee@yandex.ru>
2021-06-20 14:42:23 +0300

    add claps functionality

--- src/components/VPost.vue
+++ src/components/VPost.vue
@@ -3,15 +3,25 @@
     <h2 class="card-header-title">{{postData.title}}</h2>
     <div class="card-content">
       <div class="content">{{postData.description}}.</div>
+      <div class="content" @click="clapPost(postData.id)">Количество хлопков: {{postData.claps}}</div>
     </div>
-    <div class="card-footer"></div>
+    <div class="card-footer">{{postData.createdAt.toLocaleString('ru', {hour: 'numeric', minute: 'numeric', second: 'numeric'})}}</div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   props: {
     postData: Object
+  },
+  methods: {
+    clapPost(postId) {
+      axios.patch('http://localhost:3000/posts/' + postId, {
+        claps: this.postData.claps ++
+      })
+    }
   }
 }
 </script>
